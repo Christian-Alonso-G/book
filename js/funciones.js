7dd178bf5557af1e566bf2f221637a9ced46f440
Christian-Alonso-G <73975846+Christian-Alonso-G@users.noreply.github.com>
2020-11-04 22:47:25 -0600

    Add files via upload

---
+++ js/funciones.js
@@ -0,0 +1,203 @@
+// JavaScript Document
+
+$(document).ready(
+
+	function() {
+		
+//pantalla 1		
+		
+$("#bot_elearning").click(function (){
+			   $("#pantalla1").hide();
+			   $("#pantalla2").show();
+				});	
+				
+$("#bot_ilustracion").click(function (){
+			   $("#pantalla1").hide();
+			   $("#pantalla7").show();
+				});	
+				
+$("#bot_diseno").click(function (){
+			   $("#pantalla1").hide();
+			   $("#pantalla8").show();
+				});									
+		
+//pantalla 2
+	
+$("#regresa1").click(function (){
+	$("#pantalla2").hide();
+	$("#pantalla1").show();
+	});
+	
+$("#inicio1").click(function (){
+	$("#pantalla2").hide();
+	$("#pantalla1").show();
+	});	
+	
+$("#avanza1").click(function (){
+	$("#pantalla2").hide();
+	$("#pantalla3").show();
+	});	
+	
+//pantalla 3
+	
+$("#regresa2").click(function (){
+	$("#pantalla3").hide();
+	$("#pantalla2").show();
+	});
+	
+$("#inicio2").click(function (){
+	$("#pantalla3").hide();
+	$("#pantalla1").show();
+	});	
+	
+$("#avanza2").click(function (){
+	$("#pantalla3").hide();
+	$("#pantalla4").show();
+	});		
+	
+//pantalla 4
+	
+$("#regresa3").click(function (){
+	$("#pantalla4").hide();
+	$("#pantalla3").show();
+	});
+	
+$("#inicio3").click(function (){
+	$("#pantalla4").hide();
+	$("#pantalla1").show();
+	});	
+	
+$("#avanza3").click(function (){
+	$("#pantalla4").hide();
+	$("#pantalla5").show();
+	});
+	
+//pantalla 5
+	
+$("#regresa4").click(function (){
+	$("#pantalla5").hide();
+	$("#pantalla4").show();
+	});
+	
+$("#inicio4").click(function (){
+	$("#pantalla5").hide();
+	$("#pantalla1").show();
+	});	
+	
+$("#avanza4").click(function (){
+	$("#pantalla5").hide();
+	$("#pantalla6").show();
+	});		
+
+//pantalla 6
+	
+$("#regresa5").click(function (){
+	$("#pantalla6").hide();
+	$("#pantalla5").show();
+	});
+	
+$("#inicio5").click(function (){
+	$("#pantalla6").hide();
+	$("#pantalla1").show();
+	});	
+	
+$("#avanza5").click(function (){
+	$("#pantalla6").hide();
+	$("#pantalla1").show();
+	});	
+	
+//pantalla 7
+	
+$("#regresa6").click(function (){
+	$("#pantalla7").hide();
+	$("#pantalla1").show();
+	});
+	
+$("#inicio6").click(function (){
+	$("#pantalla7").hide();
+	$("#pantalla1").show();
+	});	
+	
+$("#avanza6").click(function (){
+	$("#pantalla7").hide();
+	$("#pantalla1").show();
+	});		
+	
+//pantalla 8
+	
+$("#regresa7").click(function (){
+	$("#pantalla8").hide();
+	$("#pantalla1").show();
+	});
+	
+$("#inicio7").click(function (){
+	$("#pantalla8").hide();
+	$("#pantalla1").show();
+	});	
+	
+$("#avanza7").click(function (){
+	$("#pantalla8").hide();
+	$("#pantalla9").show();
+	});
+	
+//pantalla 9
+	
+$("#regresa8").click(function (){
+	$("#pantalla9").hide();
+	$("#pantalla8").show();
+	});
+	
+$("#inicio8").click(function (){
+	$("#pantalla9").hide();
+	$("#pantalla1").show();
+	});	
+	
+$("#avanza8").click(function (){
+	$("#pantalla9").hide();
+	$("#pantalla10").show();
+	});	
+	
+//pantalla 10
+	
+$("#regresa9").click(function (){
+	$("#pantalla10").hide();
+	$("#pantalla9").show();
+	});
+	
+$("#inicio9").click(function (){
+	$("#pantalla10").hide();
+	$("#pantalla1").show();
+	});	
+	
+$("#avanza9").click(function (){
+	$("#pantalla10").hide();
+	$("#pantalla11").show();
+	});			
+	
+//pantalla 11
+	
+$("#regresa10").click(function (){
+	$("#pantalla11").hide();
+	$("#pantalla10").show();
+	});
+	
+$("#inicio10").click(function (){
+	$("#pantalla11").hide();
+	$("#pantalla1").show();
+	});	
+	
+$("#avanza10").click(function (){
+	$("#pantalla11").hide();
+	$("#pantalla1").show();
+	});								
+
+		
+		
+
+																					
+	}
+	);
+	
+	
+
+   
